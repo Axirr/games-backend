828b5cf652a803b7b099938b66cf1f220b1a9713
Axirr <sherlock.scott@gmail.com>
2021-09-02 17:08:24 -0600

    Backend for basic functioning Network Shogun.

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse
 import datetime
 from django.http import JsonResponse
 import random
-from hello_world.models import RPSGame
-from hello_world.models import LoveLetterGame
 from django.core import serializers
 from django.core.serializers import serialize
+
+from hello_world.models import RPSGame
+from hello_world.models import LoveLetterGame
+from hello_world.models import ShogunGame
 
 def returnGameState(request, gameId):
     # data = serializers.serialize("json", RPSGame.objects.all())
@@ -127,3 +129,88 @@ def resetGameLoveLetter(request, gameId, deckNumber):
     game.save()
     game = serializers.serialize("json", [game,])
     return HttpResponse(game)
+
+def createGameShogun(request):
+    new_entry = ShogunGame()
+    new_entry.setup(4)
+    new_entry.save()
+    # new_entry.save()
+    # new_entry = serialize('json', [new_entry, ])
+    # print(new_entry)
+    # print(new_entry.hands)
+    # jsonObject = new_entry.jsonForm()
+    return HttpResponse(new_entry.id)
+    # new_entry.save()
+
+def resetGameShogun(request, gameId):
+    game = ShogunGame.objects.get(id=gameId)
+    game.setup(4)
+    game.save()
+    game = serializers.serialize("json", [game,])
+    return HttpResponse(game)
+
+def gameStateShogun(request, gameId):
+    game = ShogunGame.objects.get(id=gameId)
+    game = serializers.serialize("json", [game,])
+    return HttpResponse(game)
+
+def rollShogun(request, gameId, playerNumber):
+    game = ShogunGame.objects.get(id=gameId)
+    response = game.roll(playerNumber)
+    game.save()
+    return HttpResponse(response)
+
+def checkIdShogun(request, gameId):
+    data = ShogunGame.objects.filter(id=gameId)
+    if (len(data) > 0): 
+        return HttpResponse("ID GOOD")
+    else:
+        return HttpResponse("ID BAD")
+
+def toggleSaveShogun(request, gameId, saveIndex):
+    game = ShogunGame.objects.get(id=gameId)
+    response = game.toggleSave(saveIndex)
+    game.save()
+    return HttpResponse(response)
+
+def resolveRollShogun(request, gameId):
+    game = ShogunGame.objects.get(id=gameId)
+    response = game.resolveRoll()
+    game.save()
+    return HttpResponse(response)
+
+def buyShogun(request, gameId, buyNumber):
+    game = ShogunGame.objects.get(id=gameId)
+    response = game.buy(buyNumber)
+    game.save()
+    return HttpResponse(response)
+
+def doneYieldingShogun(request, gameId):
+    game = ShogunGame.objects.get(id=gameId)
+    response = game.doneYielding()
+    game.save()
+    return HttpResponse(response)
+
+def setMaxHealthShogun(request, gameId, maxHealth):
+    game = ShogunGame.objects.get(id=gameId)
+    response = game.setMaxHealth(maxHealth)
+    game.save()
+    return HttpResponse(response)
+
+def setMaxVictoryShogun(request, gameId, maxVictory):
+    game = ShogunGame.objects.get(id=gameId)
+    response = game.setMaxVictoryPoints(maxVictory)
+    game.save()
+    return HttpResponse(response)
+
+def yieldEdoShogun(request, gameId, edoString):
+    game = ShogunGame.objects.get(id=gameId)
+    response = game.yieldEdo(edoString)
+    game.save()
+    return HttpResponse(response)
+    
+def clearBuyShogun(request, gameId):
+    game = ShogunGame.objects.get(id=gameId)
+    response = game.clearBuy()
+    game.save()
+    return HttpResponse(response)

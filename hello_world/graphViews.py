4f1dc80db5f4280f04a8b1c300f09ccc29d39fb2
Axirr <sherlock.scott@gmail.com>
2023-03-20 13:11:48 -0600

    Added more options to graph API call.

--- hello_world/graphViews.py
+++ hello_world/graphViews.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from .evernoteDataParsing.privateSrc.mySqlGraphCall import *
+from .evernoteDataParsing.src.globalConstants import *
 
-def refreshGraph(request, dataName, timeGroup, noWeekend, minZero, graphType, normalizeData):
+def refreshGraph(request, dataName, timeGroup, noWeekend, minZero, graphType, normalizeData, noLegend, fromFirstValidDate):
     optionsDict = {}
     
     if (timeGroup == "Daily"):
@@ -18,6 +19,11 @@ def refreshGraph(request, dataName, timeGroup, noWeekend, minZero, graphType, no
         optionsDict[BOX_PLOT] = timeGroup
     elif (graphType == BAR_OPTION):
         optionsDict[BAR_OPTION] = timeGroup
+    elif (graphType == HISTOGRAM):
+        optionsDict[HISTOGRAM] = HISTOGRAM
+    elif (graphType == SIMPLE_LINEAR_REGRESSION):
+        print("graphing SLR")
+        optionsDict[SIMPLE_LINEAR_REGRESSION] = SIMPLE_LINEAR_REGRESSION
     else:
         print("defaulting to line graph")
 
@@ -25,18 +31,41 @@ def refreshGraph(request, dataName, timeGroup, noWeekend, minZero, graphType, no
     if (minZero == "YZeroMin"):  optionsDict[Y_MIN] = 0
     if (normalizeData == "NormalizeData"): optionsDict[NORMALIZE] = True
     else: print("NOT NORMALIZING")
+
+    if (noLegend == REMOVE_LEGEND):
+        optionsDict[REMOVE_LEGEND] = REMOVE_LEGEND
+    
+    if (fromFirstValidDate == FROM_FIRST_VALID_DATE):
+        optionsDict[FROM_FIRST_VALID_DATE] = FROM_FIRST_VALID_DATE
+        print("graphing from first valid date")
+
     print(optionsDict)
 
+    myDatabase = None
 
-    if "," in dataName:
-        dataNameArray = dataName.split(",")
-        print(dataNameArray)
-        fileName = sqlGraphMultiple(dataNameArray, optionsDict)
-    else:
-        # Don't think we need this async
-        print("Trying out multi graph for single graph")
-        # fileName = sqlWrapperGraphCall(dataName, optionsDict)
-        fileName = sqlGraphMultiple([dataName], optionsDict)
+    print()
+    print()
+    print()
+    myDatabase = PRIVATE_DATABASE_ARG
+    print("CONFIGURED FOR PRIVATE DATABASE. CHANGE FOR PRODUCTION")
+    print()
+    print()
+    print()
 
 
-    return HttpResponse(fileName)
+    try:
+        if "," in dataName:
+            dataNameArray = dataName.split(",")
+            if (SIMPLE_LINEAR_REGRESSION in optionsDict):
+                if (len(dataNameArray) != 2):
+                    raise(Exception("Wrong number of data series"))
+                fileName = sqlGraphRegression(dataNameArray[0], dataNameArray[1], optionsDict, optionsDict, {}, myDatabase)
+            else:
+                fileName = sqlGraphMultiple(dataNameArray, optionsDict, myDatabase)
+        else:
+            fileName = sqlGraphMultiple([dataName], optionsDict, myDatabase)
+        print("fileName %s" % fileName)
+        return HttpResponse(fileName)
+    except Exception as e:
+        print(e)
+        return HttpResponse(status=500)
